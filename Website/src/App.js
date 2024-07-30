bc840ddfe93594a539ff8f9ce32598e96d421d45
Tom <83677440+TomMc608@users.noreply.github.com>
2024-07-30 15:35:57 +1200

    added a new website with basic navagation and a page that uses a back end to do sentiment analysis

---
+++ Website/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import SentimentAnalysis from './pages/SentimentAnalysis';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/sentiment-analysis" element={<SentimentAnalysis />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
